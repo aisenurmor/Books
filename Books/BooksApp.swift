f11bdad2724957f33f81ed5e09eac2c858062406
Aise Mor <aisenurmor@gmail.com>
2025-02-13 23:12:45 +0300

    Initial commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Aise Nur Mor on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
